06a84353756f82fd71b020924d677061f5b020af
Umang Devanshu <devumang096@gmail.com>
2023-06-08 23:55:25 +0530

    Updates

---
+++ precache-manifest.105fbef025d5584a0c5337da68c7bb41.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee01786ac00283578a66",
+    "url": "/touch-typing/static/css/main.86606c62.chunk.css"
+  },
+  {
+    "revision": "ee01786ac00283578a66",
+    "url": "/touch-typing/static/js/main.ee01786a.chunk.js"
+  },
+  {
+    "revision": "520733ebf9fdeada8325",
+    "url": "/touch-typing/static/js/1.520733eb.chunk.js"
+  },
+  {
+    "revision": "d3a70c6b5086ee5a5ff0",
+    "url": "/touch-typing/static/js/runtime~main.d3a70c6b.js"
+  },
+  {
+    "revision": "6edc040ae51723766c5d76f74dceaf76",
+    "url": "/touch-typing/index.html"
+  }
+];
