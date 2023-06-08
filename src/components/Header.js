432cf557f2f6a61bd886a9be172e7eb23500d360
Umang Devanshu <devumang096@gmail.com>
2023-06-09 01:09:32 +0530

    updating css

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ export class Header extends Component {
   render() {
     return (
       <div className="header">
-        <h1 className="header__title"><span className="header__boldText">Typing</span> practicer</h1>
+        <h1 className="header__title"><span className="header__boldText">Touch</span>Typing </h1>
         <div className="header__options" onClick={() => this.props.handleToggleMenu()}>
           <FaBars className="header__options__icon"/>
         </div>
